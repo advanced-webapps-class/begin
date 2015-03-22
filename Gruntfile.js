abb6e637bea373cde70618fbe82ce82a7d491543
AJ <aj.nice@daumkakao.com>
2015-03-22 20:26:08 +0900

    package.json, grunt

---
+++ Gruntfile.js
@@ -0,0 +1,16 @@
+module.exports = function (grunt) {
+
+  grunt.initConfig({
+    'gh-pages': {
+      options: {
+        base: '_book'
+      },
+      src: ['**']
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-gh-pages');
+
+  grunt.registerTask('default', ['gh-pages']);
+
+};
