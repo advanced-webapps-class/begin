1c6effc49e986ad66e67c34adc31c99122a1eec3
AJ <aj.nice@daumkakao.com>
2015-04-01 22:28:41 +0900

    Updates

--- ajax/assets/ajax.js
+++ ajax/assets/ajax.js
@@ -1,55 +1,16 @@
-var Ajax = function(options) {
-  this.options={
-    method:'GET',
-    param:'',
-    onComplete:null,
-    onError:null,
-    asynchronous: true,
-    contentType: 'application/x-www-form-urlencoded',
-    encoding:'UTF-8'
-  }
-  Object.extend(this.options, options);
-  if(this.options.url) this.send();
-};
+//https://gist.github.com/Xeoncross/7663273
 
-Ajax.prototype={
-  getReq:function(){
-    var req=null;
-    try { req = new XMLHttpRequest(); }
-    catch(e)
-    {
-      try { req = new ActiveXObject("Msxml2.XMLHTTP"); }
-      catch(e)
-      {
-        try { req = new ActiveXObject("Microsoft.XMLHTTP"); }
-        catch(e) { }
-      }
-    }
-    return req;
-  },
-  send:function(){
-    this.req = this.getReq();
-    var op=this.options;
-    var url=op.url;
-    var param=op.param;
-    var method=op.method.toUpperCase();
-    if(method=='GET' && param) url=url+"?"+param;
-    this.req.open(method, url, op.asynchronous);
-    this.req.setRequestHeader('Content-Type', op.contentType+';charset='+op.encoding);
-
-    var self = this;
-    this.req.onreadystatechange = function() { self.onStateChange.call(self) }
-    this.req.send(method=='POST'?param:null);
-  },
-  onStateChange: function() {
-    if(this.req.readyState==4)
-    {
-      if(this.req.status=="200") this.options.onComplete(this.req);
-      else
-      {
-        if(this.options.onError) this.options.onError(this.req);
-        else alert("서버에러입니다! 잠시후에 다시 시도하세요! "+this.req.status);
-      }
-    }
+function ajax(url, callback, data, x) {
+  try {
+    x = new(this.XMLHttpRequest || ActiveXObject)('MSXML2.XMLHTTP.3.0');
+    x.open(data ? 'POST' : 'GET', url, 1);
+    x.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+    x.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+    x.onreadystatechange = function () {
+      x.readyState > 3 && callback && callback(x.responseText, x);
+    };
+    x.send(data)
+  } catch (e) {
+    window.console && console.log(e);
   }
 };
---
+++ ajax/openapi.md
@@ -0,0 +1,16 @@
+# Open API
+
+[공개 API(Open Application Programmer Interface](http://ko.wikipedia.org/wiki/%EA%B3%B5%EA%B0%9C_API)
+
+
+## Daum 오픈API
+
+[Daum Developers](https://developers.daum.net/)
+
+### API 사용해 보기
+
+1. Daum 에 가입
+1. [Daum Developers](https://developers.daum.net/) 에서 `앱` 만들기
+1. 방금 만든 `앱`에서 API키 발급 
+1. [API 사용](https://developers.daum.net/services/apis/search/web) 
+  * https://apis.daum.net/search/web?apikey={apikey}&q=카카오프렌즈&output=json
